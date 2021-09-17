fbfa1f5a4fc6c97c7fd8beeaabfd975cc03087b7
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2021-09-18 02:43:30 +0900

    :sparkles: Update: URLのサイレント書き換えを追加

--- src/site/launcher.ts
+++ src/site/launcher.ts
@@ -8,6 +8,24 @@ license header
 
 // define functions
 
+function changeExtra(newPath: string): void {
+  history.replaceState("", "", `${extra.top()}launcher/${newPath}`);
+  extra.path = newPath;
+}
+
+let tab: string = "games";
+let item: string | null = null;
+function switchTab(tabName: string) {
+  tab = tabName;
+  item = null;
+  changeExtra(`${tab}`);
+}
+
+function switchItem(itemName: string) {
+  item = itemName;
+  changeExtra(`${tab}/${item}`);
+}
+
 // register events
 
 window.onload = event => {
